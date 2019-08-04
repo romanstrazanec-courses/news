6fe2a2a600acd39ca06aa126df911aafff4ce585
romanstrazanec <roman.strazanec007@gmail.com>
2019-08-04 14:20:11 +0200

    create repository

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+
+import '../models/item_model.dart';
+
+class Repository {
+  NewsDbProvider dbProvider = NewsDbProvider();
+  NewsApiProvider apiProvider = NewsApiProvider();
+
+  fetchTopIds() => apiProvider.fetchTopIds();
+
+  fetchItem(int id) async {
+    var item = await dbProvider.fetchItem(id);
+
+    if (item != null) return item;
+
+    item = await apiProvider.fetchItem(id);
+    dbProvider.addItem(item);
+    return item;
+  }
+}
