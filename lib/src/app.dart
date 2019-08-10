34bea52b25ff4f673d9d0cbbd7801cb308800274
romanstrazanec <roman.strazanec007@gmail.com>
2019-08-10 20:15:40 +0200

    connect comments provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'screens/news_detail.dart';
 import 'blocs/stories_provider.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        onGenerateRoute: routes,
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News!',
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
@@ -20,7 +23,11 @@ class App extends StatelessWidget {
         ? MaterialPageRoute(builder: (BuildContext context) => NewsList())
         : MaterialPageRoute(
             builder: (BuildContext context) {
+              final commentsBloc = CommentsProvider.of(context);
               final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+              commentsBloc.fetchItemWithComments(itemId);
+
               return NewsDetail(itemId: itemId);
             },
           );
